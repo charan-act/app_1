50e98e26078d8905e407e575dc5ce0f4fa6f621f
charan <charantejauti@gmail.com>
2025-03-12 22:34:22 +0530

    first commit

---
+++ application.java
@@ -0,0 +1,19 @@
+
+public class application {
+
+	public static void main(String[] args) {
+		String product="milk";
+		
+		int    price=16;
+        int    quatity=6;
+        double total=price*quatity;
+        System.out.println("product application");
+        System.out.println(product);
+        System.out.println(quatity);
+        System.out.println(price);
+        System.out.println(quatity);
+        System.out.println(total);
+        System.out.println("thank you");
+	}
+
+}
